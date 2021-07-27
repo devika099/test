77f2f1bef5cf0f6f1d7008d8ec273e5c78e6bea6
devika099 <devikamudhiraj685@gmail.com>
2021-07-27 23:35:58 +0530

    Radius of a circle

--- app/src/main/java/com/example/testdemo/MainActivity.kt
+++ app/src/main/java/com/example/testdemo/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.testdemo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.attr.button
 import android.os.Bundle
+import android.text.Editable
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var input_radius=findViewById<EditText>(R.id.input_radius)
+        var btn=findViewById<Button>(R.id.btn)
+        var txt=findViewById<TextView>(R.id.txtview)
+
+        btn.setOnClickListener(View.OnClickListener {
+            val pi:Double=3.141
+            var a:Editable = input_radius.text
+            var b:String=a.toString()
+            var radius:Double=b.toDouble()
+            var area=pi*radius*radius
+            txt.setText("Result:$area")
+        })
+
+
     }
 }
